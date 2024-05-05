22a55bfcfb29757b0a2472922847d7648d3239fa
pony <5373119@163.com>
2024-05-05 13:58:10 +0800

    完成图片提交，存储和人脸检测

--- face_detect.py
+++ face_detect.py
@@ -3,7 +3,7 @@ from fastapi.responses import JSONResponse
 from typing import Annotated
 import uvicorn
 import face_recognition
-import shutil
+import json
 import os
 
 app = FastAPI()
@@ -18,7 +18,7 @@ def allowed_file(filename):
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.get("/")
+@app.get("/", summary="仅测试用接口")
 async def root():
     return {"message": "caonima"}
 
@@ -26,18 +26,17 @@ async def root():
 #接收一个名为 file 的参数，该参数的类型是 UploadFile，
 #我们使用 File 函数将其作为请求体中的文件进行解析。
 # File 函数的第一个参数是文件类型的默认值，... 表示必须传递该参数，否则将会返回错误响应。
-@app.post('/upload_image/')
+@app.post('/upload_image/', summary="图像上传接口")
 async def upload_image(file: UploadFile = File(...)):
-    # 检查文件格式是否正确
+    # 先检查文件格式是否正确
     if False == allowed_file(file.filename):
-        print(f"upload filename is {file.filename}")
-        return JSONResponse({"message": "File format is wrong"})
+        return JSONResponse({"code": "Error", "msg": "File format is wrong"})
     #图片应该合格后再存储
     # 确保上传目录存在
     try:
         # 存储到指定的目录  返回值为空
         save_path = "E:\\test"
-        save_name = "456" + ".png"
+        save_name = "789" + ".png"
         print(os.makedirs(save_path, exist_ok=True))
         # 保存文件到服务器的指定目录并重命名
         file_path = os.path.join(save_path, save_name)
@@ -47,84 +46,52 @@ async def upload_image(file: UploadFile = File(...)):
             buffer.write(content)
     except Exception as e:
         # 返回失败的响应
-        return JSONResponse({"error": str(e)}, status_code=500)
-    # 返回一个响应
-    return JSONResponse(content={"filename": save_name, "size": file.size})
-
-    # # 检测图片是否上传成功
-    # if 'file' not in request.files:
-    #     return redirect(request.url)
-
-    # file = request.files['file']
-
-    # if file.filename == '':
-    #     return redirect(request.url)
-
-    # if file and allowed_file(file.filename):
-    #     print(
-    #         f"pony: **************** upload filename is {file.filename}******************"
-    #     )
-    #     # 图片上传成功，检测图片中的人脸
-    #     return detect_faces_in_image(file)
-    # return "jiuwfowwefoweo"
-    # return {"filename": file.filename}
-    # if file and allowed_file(file.filename):
-    #     print(f"upload filename is {file.filename}")
-    #     return JSONResponse({"message": "File uploaded successfully"})
+        return JSONResponse({"code": "Error", "msg": str(e)})
+    #这个时候文件还没读取
+    code, msg = detect_faces_in_image(file_path)
+    if code == "Error":
+        try:
+            os.remove(file_path)
+            print(f"文件 {file_path} 已被删除。")
+        except OSError as e:
+            print(f"无法删除文件: {e}")
+        finally:
+            return JSONResponse({"code": "Error", "msg": msg})
+    return JSONResponse({"code": code, "msg": msg})
 
 
+# 人脸图片检测
 def detect_faces_in_image(file_stream):
-
     # 载入用户上传的图片
     img = face_recognition.load_image_file(file_stream)
-
     shape = img.shape
-
     img_width = shape[0]
-
     img_height = shape[1]
 
     #图像宽度和高度
-    print(f"pony: img.width is {img_width},height is {img_height}")
-
-    #定位第一个人脸   这里要做异常校验
-
+    print(f"img.width is {img_width},height is {img_height}")
+    #定位第一个人脸  这里要做异常校验
     face_locations = face_recognition.face_locations(img)
-
-    print(f"pony: face_locations is  {face_locations}")
-
+    print(f"face_locations is  {face_locations}")
     if face_locations == []:
-
-        print(f"pony: there is no face in the img")
-
-        return "there is no face in the img"
+        return "Error", "未在图片中检测到人脸"
 
     if len(face_locations) > 1:
-
-        print(f"pony: there is more than one face in the img")
-
-        return "there is more than one face in the img"
-
+        return "Error", "在图片中检测到多个人脸"
+    # 第一个人脸矩形的宽和高
     face_width = face_locations[0][2] - face_locations[0][0]
-
     face_height = face_locations[0][1] - face_locations[0][3]
-
-    print(f"pony: face_width is  {face_width},face_height is {face_height}")
-
+    print(f"face_width is  {face_width},face_height is {face_height}")
+    # 人脸在图像中占比
     if face_width < img_width / 5 and face_height < img_height / 5:
-
-        print(f"pony: face area is too small")
-
-        return "face area is too small"
-
+        return "Error", "人脸在图像中占比过小"
+    # 检测人脸五官
     face_landmarks_list = face_recognition.face_landmarks(
         img, face_locations=face_locations)
-
-    print(f"pony: face_landmarks_list is \n {face_landmarks_list}")
-
-    # 讲识别结果以json键值对的数据结构输出
-
-    return jsonify(face_landmarks_list)
+    print(f"face_landmarks_list is \n {face_landmarks_list}")
+    # 将识别结果以json键值对的数据结构输出
+    # 使用json.loads()方法将字符串转换为字典
+    return "Success", "图片合格"
 
 
 if __name__ == '__main__':
