8331e16d22c21b8132fef678e3bf26f0b99bc189
pony <5373119@163.com>
2024-05-26 13:43:39 +0800

    部署IP更改

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ app = FastAPI()
 app.include_router(detect,prefix="/detect",tags=["小程序请求接口"])
 app.include_router(feedback,prefix="/feedback",tags=["云端请求接口"])
 
+
 if __name__ == '__main__':
-    uvicorn.run("main:app", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0",port=8080, reload=True)
